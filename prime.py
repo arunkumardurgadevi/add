97be20bd81c69e306c8d2414c04e33330fd9b226
arunkumardurgadevi <durgaarun.nm@gmail.com>
2019-06-06 16:17:30 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,8 @@
+lower,upper = [int(x) for x in raw_input().split()]
+for num in range(lower+1,upper):
+  if num > 1:
+    for i in range(2,num):
+      if(num%i) == 0:
+        break
+      else:
+        print(num),
